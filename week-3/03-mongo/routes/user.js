64f28f23d27c249abae9ed3803c3326cc114b93f
ShivShankarDash <dashshiv20@gmail.com>
2024-01-26 10:13:48 +0530

    Completed week 4 , assignment 3

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,19 +1,50 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username : username,
+        password : password
+    })
+    res.json({
+        msg : "User created successfully"
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+     var allCourses = await Course.find({})
+     res.json({
+        courses : allCourses
+     })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+
+    const username = req.headers.username;
+    const courseId = req.params.courseId;
+
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+    res.json({
+        message: "Purchase complete!"
+    })
+
 });
+
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic

dea7582efeab4e8ccd58e1141b540f83ce86b424
ShivShankarDash <dashshiv20@gmail.com>
2024-03-04 13:22:39 +0530

    Completed week 10 Postgres Assignment

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -11,6 +11,10 @@ import { client } from "..";
  */
 export async function createTodo(userId: number, title: string, description: string) {
     
+    const sqlQuery = `INSERT INTO todos (user_id, title, description) VALUES ($1,$2,$3) RETURNING *`
+    const values = [userId,title,description]
+    const result = await client.query(sqlQuery,values)
+    return result.rows[0]
 }
 /*
  * mark done as true for this specific todo.
@@ -23,7 +27,11 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    
+    const sqlQuery = `UPDATE todos SET done=$2 where id=$1 RETURNING *`
+    const values = [todoId,true]
+    const result = await client.query(sqlQuery,values)
+    return result.rows[0]
 }
 
 /*
@@ -37,5 +45,9 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+    
+    const sqlQuery = `SELECT * FROM todos where user_id=$1`
+    const values = [userId]
+    const result = await client.query(sqlQuery,values)
+    return result.rows
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -10,17 +10,31 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+   
+    const sqlQuery = `INSERT INTO USERS (username, password, name) VALUES ($1,$2,$3)`
+    const values = [username, password, name]
+    const result = await client.query(sqlQuery,values)
+    return result.rows[0];
 }
 
 /*
  * Should return the User object
  * {
- *   username: string,
+ *   username: string
  *   password: string,
  *   name: string
  * }
  */
 export async function getUser(userId: number) {
     
+    const sqlQuery  = `SELECT * FROM users WHERE id=$1`;
+    const values = [userId]
+    const result = await client.query(sqlQuery,values)
+    if(result.rows.length > 0){
+        return result.rows[0]
+    }
+    else {
+        return null
+    }
 }
+ 
